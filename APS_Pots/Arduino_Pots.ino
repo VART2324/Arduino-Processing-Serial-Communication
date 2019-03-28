78603ead288bd39fa417357309096f55a2681981
VART2324 <45951188+VART2324@users.noreply.github.com>
2019-03-28 16:20:50 -0400

    Add files via upload

---
+++ APS_Pots/Arduino_Pots.ino
@@ -0,0 +1,63 @@
+//------------------------------------------------------------
+// connect the potentiometer to analog pin A0
+int potPin0 = A0;
+int potPin1 = A1;
+
+// three characters used for Serial communication
+char START_BYTE = '*';
+char DELIMITER = ',';
+char END_BYTE = '#';
+
+//------------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // begin serial communication at 9600 baud rate
+  Serial.begin(9600);
+  
+  // send a byte to establish contact until receiver responds
+  establishContact();
+
+  // end of the setup function
+}
+
+//------------------------------------------------------------
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // assign potentiometer input to a variable
+  int potVal0 = analogRead(potPin0);
+  int potVal1 = analogRead(potPin1);
+
+  // start of input data being communicated...
+  Serial.write(START_BYTE); 
+  Serial.print(DELIMITER);
+  
+  // communicate data from potentiometer variable
+  Serial.print(potVal0);
+  Serial.print(DELIMITER);
+
+  // communicate data from potentiometer variable
+  Serial.print(potVal1);
+  Serial.print(DELIMITER);
+  
+  // communicate that communication is over
+  Serial.write(END_BYTE);
+  // send a carriage return
+  Serial.println();
+}
+
+//------------------------------------------------------------
+
+void establishContact(){
+  // while there is zero (or less) serial communication
+  // keep sending "hello" until the computer responds to the Arduino
+  while(Serial.available() <= 0){
+    Serial.println("hello");
+    delay(300);
+  }
+}
+
+//------------------------------------------------------------
